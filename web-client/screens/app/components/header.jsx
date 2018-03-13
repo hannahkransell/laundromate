1cb515edacff1fa887ae95033eda2ebdac2bc821
Hannah Kransell <hannah.kransell@tictail.com>
2018-03-13 18:49:13 +0100

    Add bootstrap and fix styling

--- web-client/screens/app/components/header.jsx
+++ web-client/screens/app/components/header.jsx
@@ -4,11 +4,8 @@ class Header extends React.Component {
 	render() {
 		return (
 			<div className="row beige">
-				<div className="col-xs-4">
-					<img className="img-responsive" src="https://media.giphy.com/media/l2QZUkF4YilI5fO6I/giphy.gif" />
-				</div>
-				<div className="col-xs-8">
-					<h1 className="text3d">Laundromate 5</h1>
+				<div className="col-xs-12">
+					<h1 className="text3d">Laundromate 7</h1>
 				</div>
 			</div>
 		)
